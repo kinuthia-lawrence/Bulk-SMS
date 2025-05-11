7f93d4353e813c4313c943c448d9d3fcc365e34b
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-05-12 02:53:07 +0300

    Initial commit

---
+++ src/main/java/com/larrykin/BulkSms/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.larrykin.BulkSms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Tailwind bbb";
+    }
+}
